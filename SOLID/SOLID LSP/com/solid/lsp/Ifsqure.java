cb7f91cffa8bbe56a0813761a62a258a6c06c8f6
THANATATH ASSAVARATTANAKORN <62015048@kmitl.ac.th>
2021-10-03 06:04:29 +0700

    update SOLID

---
+++ SOLID/SOLID LSP/com/solid/lsp/Ifsqure.java
@@ -0,0 +1,5 @@
+package com.solid.lsp;
+public interface Ifsqure{
+    public int getArea();
+    public int setSide(int side);
+}
--- SOLID/SOLID LSP/com/solid/lsp/Square.java
+++ SOLID/SOLID LSP/com/solid/lsp/Square.java
@@ -1,17 +1,16 @@
 package com.solid.lsp;
 
-public class Square extends Rectangle  {
+public class Square extends Rectangle implements Ifsqure {
     private int side;
     Square(int side) {
         this.setSide(side);
     }
 
-    @Override
     public int getArea() {
         return this.side * this.side;
     }
 
-    public void setSide(int side) {
+    public int setSide(int side) {
         this.side = side;
     }
 }
