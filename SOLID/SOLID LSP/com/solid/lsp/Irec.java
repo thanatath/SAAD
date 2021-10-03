a2a4b0abf8c85e8a97b67a8c8babcdf74213e116
THANATATH ASSAVARATTANAKORN <62015048@kmitl.ac.th>
2021-10-03 23:55:54 +0700

    update Solid Rec

---
+++ SOLID/SOLID LSP/com/solid/lsp/Irec.java
@@ -0,0 +1,9 @@
+package com.solid.lsp;
+
+public interface Irec{
+    public int getWidth();
+    public void setWidth(int width);
+    public int getHeight();
+    public void setHeight(int height);
+    public  int getArea();
+}
