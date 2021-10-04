ec147cc913c29f510c4fc7668e8514ae745e37ef
THANATATH ASSAVARATTANAKORN <62015048@kmitl.ac.th>
2021-10-04 21:09:08 +0700

    fix new method for isp

--- SOLID/SOLID LSP/com/solid/lsp/Irec.java
+++
@@ -1,9 +0,0 @@
-package com.solid.lsp;
-
-public interface Irec{
-    public int getWidth();
-    public void setWidth(int width);
-    public int getHeight();
-    public void setHeight(int height);
-    public  int getArea();
-}
